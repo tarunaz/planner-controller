0ed040da83e288030786f0f2ec47ee4b33638d2e
jainmnsh <jainmnsh@yahoo.com>
2017-08-09 09:11:29 -0700

    pipeline changes

--- jenkins/pipelines-plannerController-dev.groovy
+++ jenkins/pipelines-plannerController-dev.groovy
@@ -11,11 +11,11 @@ node('nodejs') {
    stage('OpenShift Build') {
      sh """
          oc project optaplanner-amq-dev
-     	 oc start-build node-amq --wait -n optaplanner-amq-dev
+     	 oc start-build planner-controller --wait -n optaplanner-amq-dev
      """
    }
     stage('OpenShift Deployment') {
      echo "Waiting on OpenShift Deployment..."
-     openshiftVerifyDeployment depCfg: 'planner-scheduler', namespace: 'optaplanner-amq-dev', replicaCount: '1', verbose: 'false', verifyReplicaCount: 'true', waitTime: '300', waitUnit: 'sec'
+     openshiftVerifyDeployment depCfg: 'planner-controller', namespace: 'optaplanner-amq-dev', replicaCount: '1', verbose: 'false', verifyReplicaCount: 'true', waitTime: '300', waitUnit: 'sec'
    }
 }
